f3848b55fca626a161eabd011473c205679658a5
lubuntu <lubuntu@lubuntu-VirtualBox.(none)>
2013-02-07 15:07:39 +0200

    initial commit

---
+++ pages/signup.php
@@ -0,0 +1,5 @@
+<form method="post" action=?page=regatud>
+	Eesnimi<input type="text" name="Fname"><br>
+	Perenimi<input type="text" name="Lname"><br>
+	e-mail<input type="text" name="email"><br>
+    <input type="submit" name="Esita">
